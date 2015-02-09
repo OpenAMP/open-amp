f77424435724459823e053997323e0341cdfddb1
Wendy Liang <jliang@xilinx.com>
2015-02-08 16:15:00 -0800

    echo_test.c: disable dcache for the program.
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- apps/tests/remote/baremetal/r5_example/echo_test.c
+++ apps/tests/remote/baremetal/r5_example/echo_test.c
@@ -19,7 +19,7 @@ static void sleep();
 static void init_system();
 
 /* Globals */
-char DBG_MSG[1024] = {1, 2, 3, 4} ; 
+char DBG_MSG[256] = {1, 2, 3, 4} ; 
 static struct rpmsg_channel *app_rp_chnl;
 static struct rpmsg_endpoint *rp_ept;
 static struct remote_proc *proc = NULL;
@@ -28,6 +28,7 @@ extern const struct remote_resource_table resources;
 
 /* Application entry point */
 int main() {
+
 	
 	/* Switch to System Mode */
 	SWITCH_TO_SYS_MODE();
@@ -35,35 +36,20 @@ int main() {
 	/* Initialize HW system components */
 	init_system();
 
+	Xil_DCacheDisable();
+
 	rsc_info.rsc_tab = (struct resource_table *)&resources;
 	rsc_info.size = sizeof(resources);
 	sprintf(DBG_MSG, "i am in main function.\n");
 
 	remoteproc_resource_init(&rsc_info, rpmsg_channel_created, rpmsg_channel_deleted, rpmsg_read_cb,
 		&proc);
-#if 0
-/* Test Interrupt Only */
-#define XSCUGIC_SPI_CPU0_MASK 1
-#define INTC_DEVICE_INT_ID 14
-#define XSCUGIC_SFI_TRIG_CPU_MASK  0x00FF0000U    /**< CPU Target list */
-#define XSCUGIC_SFI_TRIG_INTID_MASK        0x0000000FU /**< Set to the INTID signaled to the CPU */ 
-
-	unsigned int int_mask = ((XSCUGIC_SPI_CPU0_MASK << 16U) | INTC_DEVICE_INT_ID) &
-			(XSCUGIC_SFI_TRIG_CPU_MASK | XSCUGIC_SFI_TRIG_INTID_MASK);
-
-	XScuGic_DistWriteReg(XSCUGIC_SFI_TRIG_OFFSET, int_mask);
-#endif
-#if 0
-/* Test IPI */
-     HIL_MEM_WRITE32((0xff300000 + IPI_TRIG_OFFSET), 0x100);
-#endif
-
 	while (1);
+	return 0;
 }
 
 static void rpmsg_channel_created(struct rpmsg_channel *rp_chnl) {
 	app_rp_chnl = rp_chnl;
- //sprintf(DBG_MSG, "i am in %s.\n", __func__);
 	rp_ept = rpmsg_create_ept(rp_chnl, rpmsg_read_cb, RPMSG_NULL,
 			RPMSG_ADDR_ANY);
 }
@@ -73,7 +59,6 @@ static void rpmsg_channel_deleted(struct rpmsg_channel *rp_chnl) {
 
 static void rpmsg_read_cb(struct rpmsg_channel *rp_chnl, void *data, int len,
 		void * priv, unsigned long src) {
-	sprintf(DBG_MSG, "i am in %s.\n", __func__);
 	if ((*(int *) data) == SHUTDOWN_MSG) {
 		remoteproc_resource_deinit(proc);
 	} else {
