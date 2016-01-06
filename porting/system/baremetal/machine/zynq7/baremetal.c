1af58336bfefe9468e9191f2e9aa99a8bfb2736f
Wendy Liang <jliang@xilinx.com>
2016-01-06 15:50:57 -0800

    Include zynq7 baremetal.c in the OpenAMP lib
    Include the zynq7 baremetal in the OpenAMP lib
    instead of building it as a separate lib.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- porting/system/baremetal/machine/zynq7/baremetal.c
+++ porting/system/baremetal/machine/zynq7/baremetal.c
@@ -31,7 +31,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "baremetal.h"
-#include "../../../../porting/env/env.h"
+#include "openamp/env.h"
 unsigned char ARM_AR_ISR_IRQ_Data[ARM_AR_ISR_STACK_SIZE];
 unsigned char ARM_AR_ISR_FIQ_Data[ARM_AR_ISR_STACK_SIZE];
 unsigned char ARM_AR_ISR_SUP_Stack[ARM_AR_ISR_STACK_SIZE];
