f0a5e07fa6d7049239ad4a1d0b8a8fa40b51f24e
Wendy Liang <jliang@xilinx.com>
2016-01-06 16:39:08 -0800

    Remove including platform.h from platform_info.c
    It is not required to include the platform.h in the
    platform_info.c file.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- apps/common/system/baremetal/machine/zynq7/platform_info.c
+++ apps/common/system/baremetal/machine/zynq7/platform_info.c
@@ -39,7 +39,7 @@
  *
  **************************************************************************/
 
-#include "platform.h"
+#include "openamp/hil.h"
 
 /* Reference implementation that show cases platform_get_cpu_info and 
  platform_get_for_firmware API implementation for Bare metal environment */
