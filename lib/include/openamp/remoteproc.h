b6fc9b35807e9d30537567e6947b86402faac629
Wendy Liang <jliang@xilinx.com>
2016-07-01 10:54:08 -0700

    Use standard types in resource table
    Resource table fields are declared as unsigned int or char.
    As the lengths of these types varies between different platforms,
    reousrce table may change shape. Change these fields to uint32_t
    or uint8_t.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- lib/include/openamp/remoteproc.h
+++ lib/include/openamp/remoteproc.h
@@ -66,10 +66,10 @@
  */
 OPENAMP_PACKED_BEGIN
 struct resource_table {
-	unsigned int ver;
-	unsigned int num;
-	unsigned int reserved[2];
-	unsigned int offset[0];
+	uint32_t ver;
+	uint32_t num;
+	uint32_t reserved[2];
+	uint32_t offset[0];
 } OPENAMP_PACKED_END;
 
 /**
@@ -83,8 +83,8 @@ struct resource_table {
  */
 OPENAMP_PACKED_BEGIN
 struct fw_rsc_hdr {
-	unsigned int type;
-	unsigned char data[0];
+	uint32_t type;
+	uint8_t data[0];
 } OPENAMP_PACKED_END;
 
 /**
@@ -162,13 +162,13 @@ enum fw_resource_type {
  */
 OPENAMP_PACKED_BEGIN
 struct fw_rsc_carveout {
-	unsigned int type;
-	unsigned int da;
-	unsigned int pa;
-	unsigned int len;
-	unsigned int flags;
-	unsigned int reserved;
-	unsigned char name[32];
+	uint32_t type;
+	uint32_t da;
+	uint32_t pa;
+	uint32_t len;
+	uint32_t flags;
+	uint32_t reserved;
+	uint8_t name[32];
 } OPENAMP_PACKED_END;
 
 /**
@@ -202,13 +202,13 @@ struct fw_rsc_carveout {
  */
 OPENAMP_PACKED_BEGIN
 struct fw_rsc_devmem {
-	unsigned int type;
-	unsigned int da;
-	unsigned int pa;
-	unsigned int len;
-	unsigned int flags;
-	unsigned int reserved;
-	unsigned char name[32];
+	uint32_t type;
+	uint32_t da;
+	uint32_t pa;
+	uint32_t len;
+	uint32_t flags;
+	uint32_t reserved;
+	uint8_t name[32];
 } OPENAMP_PACKED_END;
 
 /**
@@ -229,11 +229,11 @@ struct fw_rsc_devmem {
  */
 OPENAMP_PACKED_BEGIN
 struct fw_rsc_trace {
-	unsigned int type;
-	unsigned int da;
-	unsigned int len;
-	unsigned int reserved;
-	unsigned char name[32];
+	uint32_t type;
+	uint32_t da;
+	uint32_t len;
+	uint32_t reserved;
+	uint8_t name[32];
 } OPENAMP_PACKED_END;
 
 /**
@@ -255,11 +255,11 @@ struct fw_rsc_trace {
  */
 OPENAMP_PACKED_BEGIN
 struct fw_rsc_vdev_vring {
-	unsigned int da;
-	unsigned int align;
-	unsigned int num;
-	unsigned int notifyid;
-	unsigned int reserved;
+	uint32_t da;
+	uint32_t align;
+	uint32_t num;
+	uint32_t notifyid;
+	uint32_t reserved;
 } OPENAMP_PACKED_END;
 
 /**
@@ -299,15 +299,15 @@ struct fw_rsc_vdev_vring {
  */
 OPENAMP_PACKED_BEGIN
 struct fw_rsc_vdev {
-	unsigned int type;
-	unsigned int id;
-	unsigned int notifyid;
-	unsigned int dfeatures;
-	unsigned int gfeatures;
-	unsigned int config_len;
-	unsigned char status;
-	unsigned char num_of_vrings;
-	unsigned char reserved[2];
+	uint32_t type;
+	uint32_t id;
+	uint32_t notifyid;
+	uint32_t dfeatures;
+	uint32_t gfeatures;
+	uint32_t config_len;
+	uint8_t status;
+	uint8_t num_of_vrings;
+	uint8_t reserved[2];
 	struct fw_rsc_vdev_vring vring[0];
 } OPENAMP_PACKED_END;
 
