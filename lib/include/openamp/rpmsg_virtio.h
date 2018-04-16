83ccd113b35719d385ce2bab9c51edef38f55c72
Kumar Gala <kumar.gala@linaro.org>
2018-04-16 08:58:37 -0500

    lib: rpmsg: Add new rpmsg
    - Add new API
    - rmpsg_init implementation
    - rpmsg: suppress the channel management
    	Rebase rpmsg on endpoint only.
    	Reintroduce rpmsg core to split interface part with the core.
    	Now rpmsg.c provide the API relying on the core.
    - add remoteproc_vshm_pool to rpmsg.h for now, we need to clean this up
    - rpmsg: add shm_io as input arguemnt to rpmsg_vdev_init
    - Add core function to unregister the end points.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Wendy Liang <jliang@xilinx.com>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ lib/include/openamp/rpmsg_virtio.h
@@ -0,0 +1,101 @@
+/*
+ * rpmsg based on virtio
+ *
+ * Copyright (C) 2018 Linaro, Inc.
+ *
+ * All rights reserved.
+ * Copyright (c) 2016 Freescale Semiconductor, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _RPMSG_VIRTIO_H_
+#define _RPMSG_VIRTIO_H_
+
+#include <metal/io.h>
+#include <metal/mutex.h>
+#include <openamp/rpmsg.h>
+#include <openamp/virtio.h>
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+/* Configurable parameters */
+#ifndef RPMSG_BUFFER_SIZE
+#define RPMSG_BUFFER_SIZE	(512)
+#endif
+
+/* The feature bitmap for virtio rpmsg */
+#define VIRTIO_RPMSG_F_NS 0 /* RP supports name service notifications */
+
+/**
+ * struct rpmsg_virtio_device - representation of a rpmsg device based on virtio
+ * @rdev: rpmsg device, first property in the struct
+ * @vdev: pointer to the virtio device
+ * @rvq: pointer to receive virtqueue
+ * @svq: pointer to send virtqueue
+ * @buffers_number: number of shared buffers
+ * @shbuf_io: pointer to the shared buffer I/O region.
+ * @new_endpoint_cb: callback handler for new service announcement without local
+ *                   endpoints waiting to bind.
+ * @endpoints: list of endpoints.
+ */
+struct rpmsg_virtio_device {
+	struct rpmsg_device rdev;
+	struct virtio_device *vdev;
+	struct virtqueue *rvq;
+	struct virtqueue *svq;
+	struct metal_io_region *shbuf_io;
+	struct sh_mem_pool *shbuf;
+};
+
+#define RPMSG_REMOTE	VIRTIO_DEV_SLAVE
+#define RPMSG_MASTER	VIRTIO_DEV_MASTER
+static inline unsigned int rpmsg_virtio_get_role(struct rpmsg_virtio_device *rvdev)
+{
+	return rvdev->vdev->role;
+}
+
+static inline void rpmsg_virtio_set_status(struct rpmsg_virtio_device *rvdev, uint8_t status)
+{
+	rvdev->vdev->func->set_status(rvdev->vdev, status);
+}
+
+static inline uint8_t rpmsg_virtio_get_status(struct rpmsg_virtio_device *rvdev)
+{
+	return rvdev->vdev->func->get_status(rvdev->vdev);
+}
+
+static inline uint32_t rpmsg_virtio_get_features(struct rpmsg_virtio_device *rvdev)
+{
+	return rvdev->vdev->func->get_features(rvdev->vdev);
+}
+
+static inline int rpmsg_virtio_create_virtqueues(struct rpmsg_virtio_device * rvdev, int flags,
+				  unsigned int nvqs, const char *names[],
+				  vq_callback * callbacks[])
+{
+	return virtio_create_virtqueues(rvdev->vdev, flags, nvqs, names, callbacks);
+}
+
+int rpmsg_init_vdev(struct rpmsg_virtio_device *rvdev,
+		    struct virtio_device *vdev,
+		    void (*new_endpoint_cb)(struct rpmsg_endpoint *ep),
+		    struct metal_io_region *shm_io,
+		    void *shm, unsigned int len);
+
+void rpmsg_deinit_vdev(struct rpmsg_virtio_device *rvdev);
+
+static inline struct rpmsg_device *
+rpmsg_virtio_get_rpmsg_device(struct rpmsg_virtio_device *rvdev)
+{
+	return &rvdev->rdev;
+}
+
+
+#if defined __cplusplus
+}
+#endif
+
+#endif	/* _RPMSG_VIRTIO_H_ */
