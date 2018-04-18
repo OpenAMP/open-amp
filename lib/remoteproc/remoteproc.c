00c45da7afd9e5fb998b8bea0dd66a62f57539f6
Wendy Liang <jliang@xilinx.com>
2018-04-18 00:15:45 -0700

    remoteproc: implement remoteproc create/remove vdev function
    Implement remoteproc function to create/remove vdev.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- lib/remoteproc/remoteproc.c
+++ lib/remoteproc/remoteproc.c
@@ -442,3 +442,72 @@ unsigned int remoteproc_allocate_id(struct remoteproc *rproc,
 		metal_bitmap_set_bit(&rproc->bitmap, nofityid);
 	return notifyid;
 }
+
+struct virtio_device *
+remoteproc_create_virtio(struct remoteproc *rproc,
+			 int vdev_id, unsigned int role,
+			 int (*rst_cb)(struct virtio_device *vdev))
+{
+	void *rsc_table;
+	struct fw_rsc_vdev *vdev_rsc;
+	struct metal_io_region *vdev_rsc_io;
+	struct virtio_device *vdev;
+	size_t vdev_rsc_offset;
+	unsigned int notifyid;
+	unsigned int num_vrings, i;
+
+	metal_assert(rproc);
+	metal_mutex_acquire(&rproc->lock);
+	rsc_table = rproc->rsc_table;
+	vdev_rsc_io = rproc->rsc_io;
+	vdev_rsc_offset = find_rsc(rsc_table, RSC_VDEV, vdev_id);
+	if (!vdev_rsc_offset) {
+		metal_mutex_release(&rproc->lock);
+		return NULL;
+	}
+	vdev_rsc = rsc_table + vdev_rsc_offset;
+	notifyid = vdev_rsc->notifyid;
+	vdev = rproc_virtio_create_vdev(role, vdev_notifyid,
+					vdev_rsc, vdev_rsc_io,
+					rproc, rproc->nofity, rst_cb);
+	num_vrings = vdev_rsc->num_of_vrings;
+	/* set the notification id for vrings */
+	for (i = 0; i < num_vrings; i++) {
+		struct fw_rsc_vdev_vring *vring_rsc;
+		metal_phys_addr_t da;
+		unsigned int notifyid, num_descs, align;
+		struct metal_io_region *io;
+		size_t size;
+		void *va;
+		int ret;
+
+		vring_rsc = &vdev_rsc->vrings[i];
+		notifyid = vring_rsc->notifyid;
+		da = vring_rsc->da;
+		num_descs = vring_rsc->num;
+		align = vring_rsc->align;
+		size = vring_size(num_descs, align);
+		va = remoteproc_datova(rproc, da, size, &io)
+		if (!va)
+			goto err1;
+		ret = rproc_virtio_init_vring(vdev, i, notifyid,
+					      va, io, num_descs, align);
+
+	}
+err1:
+	remoteproc_remove_virtio(rproc, vdev);
+	vdev = NULL;
+	metal_mutex_release(&rproc->lock);
+	return vdev;
+}
+
+void remoteproc_remove_virtio(struct remoteproc *rproc,
+			      struct virtio_device *vdev)
+{
+	struct remoteproc_virtio *rpvdev;
+
+	(void)rproc;
+	metal_assert(vdev);
+	rpvdev = metal_container_of(vdev, struct remoteproc_virtio, vdev);
+	rproc_virtio_remove_vdev(&rpvdev->vdev);
+}
