76da27ce0d4ae12ad6be1b21916729477f2d2cc3
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2018-05-29 18:12:32 +0200

    rpmsg: fix coding rules
    Fix coding rule highlighted by linux checkpatch script
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- lib/include/openamp/rpmsg_virtio.h
+++ lib/include/openamp/rpmsg_virtio.h
@@ -65,12 +65,14 @@ struct rpmsg_virtio_device {
 
 #define RPMSG_REMOTE	VIRTIO_DEV_SLAVE
 #define RPMSG_MASTER	VIRTIO_DEV_MASTER
-static inline unsigned int rpmsg_virtio_get_role(struct rpmsg_virtio_device *rvdev)
+static inline unsigned int
+	rpmsg_virtio_get_role(struct rpmsg_virtio_device *rvdev)
 {
 	return rvdev->vdev->role;
 }
 
-static inline void rpmsg_virtio_set_status(struct rpmsg_virtio_device *rvdev, uint8_t status)
+static inline void rpmsg_virtio_set_status(struct rpmsg_virtio_device *rvdev,
+					   uint8_t status)
 {
 	rvdev->vdev->func->set_status(rvdev->vdev, status);
 }
@@ -80,16 +82,20 @@ static inline uint8_t rpmsg_virtio_get_status(struct rpmsg_virtio_device *rvdev)
 	return rvdev->vdev->func->get_status(rvdev->vdev);
 }
 
-static inline uint32_t rpmsg_virtio_get_features(struct rpmsg_virtio_device *rvdev)
+static inline uint32_t
+	rpmsg_virtio_get_features(struct rpmsg_virtio_device *rvdev)
 {
 	return rvdev->vdev->func->get_features(rvdev->vdev);
 }
 
-static inline int rpmsg_virtio_create_virtqueues(struct rpmsg_virtio_device * rvdev, int flags,
-				  unsigned int nvqs, const char *names[],
-				  vq_callback * callbacks[])
+static inline int
+	rpmsg_virtio_create_virtqueues(struct rpmsg_virtio_device *rvdev,
+				       int flags, unsigned int nvqs,
+				       const char *names[],
+				       vq_callback * callbacks[])
 {
-	return virtio_create_virtqueues(rvdev->vdev, flags, nvqs, names, callbacks);
+	return virtio_create_virtqueues(rvdev->vdev, flags, nvqs, names,
+					callbacks);
 }
 
 /**
@@ -153,7 +159,6 @@ rpmsg_virtio_get_rpmsg_device(struct rpmsg_virtio_device *rvdev)
 {
 	return &rvdev->rdev;
 }
-
 
 #if defined __cplusplus
 }
