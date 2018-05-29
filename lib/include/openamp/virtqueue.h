3ad00ec42f50f0808b0e2639dacc933adca4249e
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2018-05-29 18:49:41 +0200

    virtio: fix coding rules
    Fix coding rule highlighted by linux checkpatch script
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- lib/include/openamp/virtqueue.h
+++ lib/include/openamp/virtqueue.h
@@ -74,12 +74,12 @@ struct virtqueue {
 	uint16_t vq_queue_index;
 	uint16_t vq_nentries;
 	uint32_t vq_flags;
-	void (*callback) (struct virtqueue * vq);
-	void (*notify) (struct virtqueue * vq);
+	void (*callback)(struct virtqueue *vq);
+	void (*notify)(struct virtqueue *vq);
 	struct vring vq_ring;
 	uint16_t vq_free_cnt;
 	uint16_t vq_queued_cnt;
-	void * shm_io; /* opaque pointer to data needed to allow v2p & p2v */
+	void *shm_io; /* opaque pointer to data needed to allow v2p & p2v */
 
 	/*
 	 * Head of the free chain in the descriptor table. If
@@ -132,35 +132,36 @@ typedef void vq_notify(struct virtqueue *);
 #include <metal/assert.h>
 
 #define VQASSERT(_vq, _exp, _msg) \
-	do{ \
-		if (!(_exp)){ \
+	do { \
+		if (!(_exp)) { \
 			metal_log(METAL_LOG_EMERGENCY, \
-				  "%s: %s - "_msg, \
-				  __func__, \
-				  (_vq)->vq_name); \
+				  "%s: %s - _msg", __func__, (_vq)->vq_name); \
 			metal_assert(_exp); \
 		} \
-	} while(0)
+	} while (0)
 
 #define VQ_RING_ASSERT_VALID_IDX(_vq, _idx)            \
-    VQASSERT((_vq), (_idx) < (_vq)->vq_nentries,        \
-    "invalid ring index")
+	VQASSERT((_vq), (_idx) < (_vq)->vq_nentries, "invalid ring index")
 
 #define VQ_RING_ASSERT_CHAIN_TERM(_vq)                \
-    VQASSERT((_vq), (_vq)->vq_desc_head_idx ==            \
-    VQ_RING_DESC_CHAIN_END,    "full ring terminated incorrectly: invalid head")
+	VQASSERT((_vq), (_vq)->vq_desc_head_idx ==            \
+	VQ_RING_DESC_CHAIN_END, "full ring terminated incorrectly: invalid head")
 
-#define VQ_PARAM_CHK(condition, status_var, status_err)                 \
-                       if ((status_var == 0) && (condition))            \
-                       {                                                \
-                           status_var = status_err;                     \
-                       }
+#define VQ_PARAM_CHK(condition, status_var, status_err) \
+	do {						\
+		if ((status_var == 0) && (condition)) { \
+			status_var = status_err;        \
+		}					\
+	} while (0)
 
-#define VQUEUE_BUSY(vq)         if ((vq)->vq_inuse == false)                 \
-                                    (vq)->vq_inuse = true;                   \
-                                else                                         \
-                                    VQASSERT(vq, (vq)->vq_inuse == false,    \
-                                        "VirtQueue already in use")
+#define VQUEUE_BUSY(vq) \
+	do {						     \
+		if ((vq)->vq_inuse == false)                 \
+			(vq)->vq_inuse = true;               \
+		else                                         \
+			VQASSERT(vq, (vq)->vq_inuse == false,\
+				"VirtQueue already in use")  \
+	} while (0)
 
 #define VQUEUE_IDLE(vq)            ((vq)->vq_inuse = false)
 
@@ -178,8 +179,8 @@ typedef void vq_notify(struct virtqueue *);
 
 int virtqueue_create(struct virtio_device *device, unsigned short id,
 		     const char *name, struct vring_alloc_info *ring,
-		     void (*callback) (struct virtqueue * vq),
-		     void (*notify) (struct virtqueue * vq),
+		     void (*callback)(struct virtqueue *vq),
+		     void (*notify)(struct virtqueue *vq),
 		     struct virtqueue *v_queue);
 
 /*
@@ -199,10 +200,10 @@ static inline void virtqueue_set_shmem_io(struct virtqueue *vq,
 int virtqueue_add_buffer(struct virtqueue *vq, struct virtqueue_buf *buf_list,
 			 int readable, int writable, void *cookie);
 
-void *virtqueue_get_buffer(struct virtqueue *vq, uint32_t * len, uint16_t *idx);
+void *virtqueue_get_buffer(struct virtqueue *vq, uint32_t *len, uint16_t *idx);
 
-void *virtqueue_get_available_buffer(struct virtqueue *vq, uint16_t * avail_idx,
-				     uint32_t * len);
+void *virtqueue_get_available_buffer(struct virtqueue *vq, uint16_t *avail_idx,
+				     uint32_t *len);
 
 int virtqueue_add_consumed_buffer(struct virtqueue *vq, uint16_t head_idx,
 				  uint32_t len);
