b086bf56997a2c7baf534cd71840c6e62c111aaa
Wendy Liang <jliang@xilinx.com>
2018-06-01 13:33:34 -0700

    apps: matrix multiply: destroy endpoint when app finishes
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- apps/matrix_multiply/matrix_multiply.c
+++ apps/matrix_multiply/matrix_multiply.c
@@ -222,6 +222,7 @@ int app (struct rpmsg_device *rdev, void *priv)
 
 	/* Send shutdown message to remote */
 	rpmsg_send(&lept, &shutdown_msg, sizeof(int));
+	rpmsg_destroy_ept(&lept);
 	while(!ept_deleted)
 		platform_poll(priv);
 	LPRINTF("Quitting application .. Matrix multiplication end\n");
