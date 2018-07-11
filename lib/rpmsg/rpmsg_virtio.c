c6e0da3a67deef9d9af4200892e70315e1708aad
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2018-07-11 15:49:22 +0200

    rpmsg: clean mutex management on end point destroy
    Application can call rpmsg_destroy_ept. But mutex is not well managed in
    this function that is also called by deinit function.
    We can consider that deinit function  is called when there is no more
    expected activity.
    So the patch is to remove the mutex management in deinit, and
    handle it coorectly in rpmsg_destroy_ept.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- lib/rpmsg/rpmsg_virtio.c
+++ lib/rpmsg/rpmsg_virtio.c
@@ -479,8 +479,10 @@ static int rpmsg_virtio_ns_callback(struct rpmsg_endpoint *ept, void *data,
 	_ept = rpmsg_get_endpoint(rdev, name, RPMSG_ADDR_ANY, dest);
 
 	if (ns_msg->flags & RPMSG_NS_DESTROY) {
-		if (_ept)
+		if (_ept) {
+			metal_mutex_release(&rdev->lock);
 			rpmsg_destroy_ept(_ept);
+		}
 	} else {
 		if (!_ept) {
 			/*
@@ -492,11 +494,12 @@ static int rpmsg_virtio_ns_callback(struct rpmsg_endpoint *ept, void *data,
 			metal_mutex_release(&rdev->lock);
 			if (rdev->ns_bind_cb)
 				rdev->ns_bind_cb(rdev, name, dest);
-			return RPMSG_SUCCESS;
+		} else {
+			_ept->dest_addr = dest;
+			metal_mutex_release(&rdev->lock);
 		}
-		_ept->dest_addr = dest;
 	}
-	metal_mutex_release(&rdev->lock);
+
 	return RPMSG_SUCCESS;
 }
 
@@ -656,7 +659,6 @@ void rpmsg_deinit_vdev(struct rpmsg_virtio_device *rvdev)
 	struct rpmsg_endpoint *ept;
 
 	rdev = &rvdev->rdev;
-	metal_mutex_acquire(&rdev->lock);
 	while (!metal_list_is_empty(&rdev->endpoints)) {
 		node = rdev->endpoints.next;
 		ept = metal_container_of(node, struct rpmsg_endpoint, node);
@@ -665,8 +667,6 @@ void rpmsg_deinit_vdev(struct rpmsg_virtio_device *rvdev)
 
 	rvdev->rvq = 0;
 	rvdev->svq = 0;
-
-	metal_mutex_release(&rdev->lock);
 
 	metal_mutex_deinit(&rdev->lock);
 }
