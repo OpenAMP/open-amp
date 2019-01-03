c6007604592687ecc6825b593529c9ab6ff58bfe
Xiang Xiao <xiaoxiang@xiaomi.com>
2019-01-04 00:15:45 +0800

    remoteproc_load_noblock remove the unnecessary get_load_state call
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- lib/remoteproc/remoteproc.c
+++ lib/remoteproc/remoteproc.c
@@ -723,7 +723,6 @@ int remoteproc_load_noblock(struct remoteproc *rproc,
 		ret = loader->load_header(img_data, offset, len,
 					  &limg_info, last_load_state,
 					  noffset, nlen);
-		last_load_state = (unsigned int)ret;
 		metal_log(METAL_LOG_DEBUG,
 			  "%s, load header 0x%lx, 0x%x, next 0x%lx, 0x%x\r\n",
 			  __func__, offset, len, *noffset, *nlen);
@@ -734,7 +733,7 @@ int remoteproc_load_noblock(struct remoteproc *rproc,
 
 			goto error1;
 		}
-		last_load_state = loader->get_load_state(limg_info);
+		last_load_state = ret;
 		if (*nlen != 0 &&
 		    (last_load_state & RPROC_LOADER_READY_TO_LOAD) == 0)
 			goto out;
@@ -756,6 +755,7 @@ int remoteproc_load_noblock(struct remoteproc *rproc,
 				  offset, len);
 			goto error1;
 		}
+		last_load_state = ret;
 		if (da != RPROC_LOAD_ANYADDR) {
 			/* get the I/O region from remoteproc */
 			*pa = METAL_BAD_PHYS;
@@ -770,8 +770,6 @@ int remoteproc_load_noblock(struct remoteproc *rproc,
 		}
 		if (*nlen != 0)
 			goto out;
-		else
-			last_load_state = loader->get_load_state(limg_info);
 	}
 	if ((last_load_state & RPROC_LOADER_LOAD_COMPLETE) != 0) {
 		/* Get resource table */
