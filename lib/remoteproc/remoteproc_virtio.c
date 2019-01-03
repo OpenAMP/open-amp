eb6a05abed9c82babd9477fbf6443322d9192467
Xiang Xiao <xiaoxiang@xiaomi.com>
2019-01-03 23:07:26 +0800

    call rproc_virtio_wait_remote_ready instead the adhoc loop
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- lib/remoteproc/remoteproc_virtio.c
+++ lib/remoteproc/remoteproc_virtio.c
@@ -36,6 +36,7 @@
 #include <openamp/remoteproc.h>
 #include <openamp/remoteproc_virtio.h>
 #include <openamp/virtqueue.h>
+#include <metal/cpu.h>
 #include <metal/utilities.h>
 #include <metal/alloc.h>
 
@@ -326,5 +327,6 @@ void rproc_virtio_wait_remote_ready(struct virtio_device *vdev)
 		status = rproc_virtio_get_status(vdev);
 		if (status & VIRTIO_CONFIG_STATUS_DRIVER_OK)
 			return;
+		metal_cpu_yield();
 	}
 }
