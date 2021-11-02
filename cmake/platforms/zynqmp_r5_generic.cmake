9ef58af2986aaaf212ab746ecd0c0efe236340ed
Gaute Nilsson <gaute.nilsson@siemens-energy.com>
2021-11-02 10:28:22 +0100

    Remove vim modeline comments
    This is currently inconsistent with the rest of the project.
    
    Signed-off-by: Gaute Nilsson <gaute.nilsson@siemens-energy.com>

--- cmake/platforms/zynqmp_r5_generic.cmake
+++ cmake/platforms/zynqmp_r5_generic.cmake
@@ -6,5 +6,3 @@ set (CROSS_PREFIX           "armr5-none-eabi-" CACHE STRING "")
 set (CMAKE_C_FLAGS          "-mfloat-abi=hard -mfpu=vfpv3-d16 -mcpu=cortex-r5" CACHE STRING "")
 
 include (cross_generic_gcc)
-
-# vim: expandtab:ts=2:sw=2:smartindent
