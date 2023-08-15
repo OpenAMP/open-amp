13035180d7e51c9516b115d654935ef191b9f0b1
Kendall Willis <kendallwillis@ti.com>
2023-08-15 09:34:06 -0500

    cmake: platforms: Added libmetal library and include path to ti_k3_r5.cmake
    Allow users to run build cmake without having to specify where libmetal
    is if it is in the same directory as OpenAMP. Included LIBMETAL_PATH
    variable if the libmetal directory is not in the same directory as
    OpenAMP.
    
    Signed-off-by: Kendall Willis <kendallwillis@ti.com>

--- cmake/platforms/ti_k3_r5.cmake
+++ cmake/platforms/ti_k3_r5.cmake
@@ -2,6 +2,13 @@ set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
 set (MACHINE                "ti_k3_r5" CACHE STRING "")
 set (CROSS_PREFIX           "arm-none-eabi-" CACHE STRING "")
 
+if (NOT LIBMETAL_PATH)
+  set (LIBMETAL_PATH "../libmetal")
+endif (NOT LIBMETAL_PATH)
+
+set (CMAKE_INCLUDE_PATH     "${LIBMETAL_PATH}/build-libmetal/usr/local/include")
+set (CMAKE_LIBRARY_PATH     "${LIBMETAL_PATH}/build-libmetal/usr/local/lib")
+
 set (CMAKE_C_FLAGS          "-mcpu=cortex-r5 -g -O0" CACHE STRING "")
 
 include (cross_generic_gcc)
