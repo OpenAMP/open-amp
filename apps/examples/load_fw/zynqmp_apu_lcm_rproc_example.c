408e698180eb7c9aaebcb7057030873fbc2fbe4b
Andrew Davis <afd@ti.com>
2024-03-14 09:50:05 -0500

    apps: examples: load_fw: Do not re-init rproc mems list
    The memory list is already initialized before this callback is called.
    Remove this redundant initialization.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- apps/examples/load_fw/zynqmp_apu_lcm_rproc_example.c
+++ apps/examples/load_fw/zynqmp_apu_lcm_rproc_example.c
@@ -27,7 +27,6 @@ static struct remoteproc *apu_rproc_init(struct remoteproc *rproc,
 	priv->rproc = rproc;
 	priv->cpu_id = cpu_id;
 	priv->rproc->ops = ops;
-	metal_list_init(&priv->rproc->mems);
 	priv->rproc->priv = priv;
 	rproc->state = RPROC_READY;
 	return priv->rproc;
